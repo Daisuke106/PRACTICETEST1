7610e62b9d58e63fe6f984174c71217abbf59087
Daisuke106 <k022c2145@g.neec.ac.jp>
2023-03-24 12:28:55 +0900

    CREATE blog post edit screen

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -12,7 +12,10 @@
             <div class='content_post'>
                 <h3>本文</h3>
                 <p class='body'>{{ $post->body }}</p>
+            </div>
         </div>
+        <div class='edit'>
+            <a href="/posts/{{ $post->id }}/edit">編集</a>
         </div>
         <div class='footer'>
             <a href="/">戻る</a>
